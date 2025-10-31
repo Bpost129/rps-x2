0fdc0277aac81a64eceba169a37bf8bb760aa946
Bpost129 <Bpost129@yahoo.com>
2025-10-31 13:53:17 -0400

    basic html & css, declare variables & constants, create event listeners

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const choices = ['rock', 'paper', 'scissors']
+
+/*-------------------------------- Variables --------------------------------*/
+
+let playerChoice, cpuChoice, msg
+
+/*------------------------ Cached Element References ------------------------*/
+
+const resultDisplayEl = document.getElementById('result-display')
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+document.getElementById('rock').addEventListener('click', play)
+document.getElementById('paper').addEventListener('click', play)
+document.getElementById('scissors').addEventListener('click', play)
+
+/*-------------------------------- Functions --------------------------------*/
