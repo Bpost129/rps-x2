791fc7770eb7c057dd741c72593a8558e30320ba
Bpost129 <Bpost129@yahoo.com>
2025-10-31 14:23:33 -0400

    create play functions, render winner message

--- js/app.js
+++ js/app.js
@@ -17,3 +17,32 @@ document.querySelectorAll("button").forEach(function (button) {
 })
 
 /*-------------------------------- Functions --------------------------------*/
+
+function play(e) {
+  getPlayerChoice(e)
+  getCpuChoice()
+  compare()
+}
+
+function getPlayerChoice(e) {
+  playerChoice = e.target.id
+}
+
+function getCpuChoice() {
+  const randomIdx = Math.floor(Math.random() * choices.length)
+  cpuChoice = choices[randomIdx]
+}
+
+function compare() {
+  if (playerChoice === computerChoice) {
+    msg = 'You tied!'
+  } else if (playerChoice === choices[0] && computerChoice === choices[2]) {
+    msg = "Congrats! You win!"
+  } else if (playerChoice === choices[1] && computerChoice === choices[0]) {
+    msg = "Congrats! You win!"
+  } else if (playerChoice === choices[2] && computerChoice === choices[1]) {
+    msg = "Congrats! You win!"
+  } else {
+    msg = "You lose! Try again?"
+  }
+}
